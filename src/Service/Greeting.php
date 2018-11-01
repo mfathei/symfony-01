ee90b1db0af0d814e3f5cf54213b016737101f69
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-01 12:14:42 +0200

    13 Manual service wiring, parameter binding

--- src/Service/Greeting.php
+++ src/Service/Greeting.php
@@ -14,14 +14,16 @@ use Psr\Log\LoggerInterface;
 class Greeting
 {
     private $logger;
+    private $message;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, string $message)
     {
         $this->logger = $logger;
+        $this->message = $message;
     }
     public function greet(string $name): string
     {
         $this->logger->info("Greeted $name.");
-        return "Hello $name";
+        return "{$this->message} $name";
     }
 }
