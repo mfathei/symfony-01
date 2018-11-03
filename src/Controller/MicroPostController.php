369b9127571a67a153319ece610d1b95d159fead
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-03 10:45:59 +0200

    27 creating a form and handling a form submission

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,11 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/micro-post")
+ */
 class MicroPostController extends AbstractController
 {
     /**
@@ -19,14 +29,36 @@ class MicroPostController extends AbstractController
      */
     private $microPostRepository;
 
-    public function __construct(\Twig_Environment $twig, MicroPostRepository $microPostRepository)
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(\Twig_Environment $twig,
+                                MicroPostRepository $microPostRepository,
+                                FormFactoryInterface $formFactory,
+                                EntityManagerInterface $entityManager,
+                                RouterInterface $router
+    )
     {
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
     }
 
     /**
-     * @Route("/micro-post", name="micro_post")
+     * @Route("/", name="micro_post_index")
      */
     public function index()
     {
@@ -37,5 +69,31 @@ class MicroPostController extends AbstractController
         ]);
 
         return new Response($html);
+    }
+
+    /**
+     * @Route("/add", name="micro_post_add")
+     */
+    public function add(Request $request)
+    {
+        $microPost = new MicroPost();
+
+        $form = $this->formFactory->create(MicroPostType::class, $microPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $microPost->setTime(new \DateTime());
+
+            $this->entityManager->persist($microPost);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate('micro_post_index'));
+        }
+
+        return new Response($this->twig->render('micro-post/add.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        ));
     }
 }
