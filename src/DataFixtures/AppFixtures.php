5fe59c15f6ad083ffd9781a6f26ac7c6211201bb
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-03 09:59:11 +0200

    26 creating fixtures (database seeding)

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahdy
+ * Date: 11/3/18
+ * Time: 9:53 AM
+ */
+
+namespace App\DataFixtures;
+
+
+use App\Entity\MicroPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $microPost = new MicroPost();
+            $microPost->setText('Some random text ' . rand(0, 100));
+            $microPost->setTime(new \DateTime());
+            $manager->persist($microPost);
+        }
+
+        $manager->flush();
+    }
+}
