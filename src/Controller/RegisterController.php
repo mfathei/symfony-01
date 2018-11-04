5ff3b3cef0f8d2b240b7ebf4d8f60f5e4b225d7b
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-04 15:22:57 +0200

    40 user registration controller

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(UserPasswordEncoderInterface $passwordEncoder, Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPlainPassword()));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirect('micro-post');
+        }
+
+        return $this->render('register/register.html.twig', ['form' => $form->createView()]);
+    }
+}
