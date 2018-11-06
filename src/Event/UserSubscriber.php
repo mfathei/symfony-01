d1375c99b67ae7d03552f388ed4ddeaa5097758e
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-06 22:31:47 +0200

    87 Create new UserPreferences for the user when register(using subscriber)

--- src/Event/UserSubscriber.php
+++ src/Event/UserSubscriber.php
@@ -9,7 +9,9 @@
 namespace App\Event;
 
 
+use App\Entity\UserPreferences;
 use App\Mailer\Mailer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UserSubscriber implements EventSubscriberInterface
@@ -18,10 +20,20 @@ class UserSubscriber implements EventSubscriberInterface
      * @var Mailer
      */
     private $mailer;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var string
+     */
+    private $defaultLocale;
 
-    public function __construct(Mailer $mailer)
+    public function __construct(Mailer $mailer, EntityManagerInterface $entityManager, string $defaultLocale)
     {
         $this->mailer = $mailer;
+        $this->entityManager = $entityManager;
+        $this->defaultLocale = $defaultLocale;
     }
 
     public static function getSubscribedEvents()
@@ -33,6 +45,14 @@ class UserSubscriber implements EventSubscriberInterface
 
     public function onUserRegister(UserRegisterEvent $registerEvent)
     {
+        $preferences = new UserPreferences();
+        $preferences->setLocale($this->defaultLocale);
+        $this->entityManager->persist($preferences);
+
+        $registerEvent->getRegisteredUser()->setPreferences($preferences);
+
+        $this->entityManager->flush();
+
         $this->mailer->sendConfirmationEmail($registerEvent->getRegisteredUser());
     }
 }
