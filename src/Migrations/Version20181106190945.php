8d81e7fc181708a14d863d89572dd906ad07db81
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-06 21:22:03 +0200

    84 storing user sessions in database

---
+++ src/Migrations/Version20181106190945.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181106190945 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE `sessions`(
+          sess_id varchar(128) NOT NULL PRIMARY KEY,
+          sess_data BLOB NOT NULL,
+          sess_time INTEGER UNSIGNED NOT NULL,
+          sess_lifetime MEDIUMINT NOT NULL
+        ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE sessions');
+    }
+}
